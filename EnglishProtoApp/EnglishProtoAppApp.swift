57609bbff95c4e4cfec6e6fedea2ac3993525f4b
Morgan-Krigsman <morgan.krigsman01@gmail.com>
2024-10-22 12:20:43 +0900

    Initial Commit

---
+++ EnglishProtoApp/EnglishProtoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishProtoAppApp.swift
+//  EnglishProtoApp
+//
+//  Created by Morgan Krigsman on 22.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct EnglishProtoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
